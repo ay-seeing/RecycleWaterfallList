df1fa7e900c047ed65cfd6e040d8594b0ffc8421
yiyang <630999015@qq.com>
2022-08-10 09:49:30 +0800

    feat: 组件接收的属性定义时只能是一级，不能是多级，否则会报错

--- component/RecycleWaterfallList/RecycleWaterfallList.js
+++ component/RecycleWaterfallList/RecycleWaterfallList.js
@@ -2,7 +2,7 @@
  * @Author: yiyang 630999015@qq.com
  * @Date: 2022-07-18 10:49:45
  * @LastEditors: yiyang 630999015@qq.com
- * @LastEditTime: 2022-08-09 16:12:31
+ * @LastEditTime: 2022-08-10 09:47:44
  * @FilePath: /WeChatProjects/ComponentLongList/component/RecycleList/RecycleList.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -53,18 +53,12 @@ Component({
     },
     properties: {
         apiInfo: {   // api相关信息
-            url: {
-                type: String,
-                value: '',
-            },
-            apiData: {  // 除翻页外的其他接口参数，但不包含 offset 和 limit
-                type: Object,
-                value: {},
-            },
-            count: {  // 每页几个
-                type: Number,
-                value: 30,
-            },
+            type: Object,
+            value: {
+                url: '',
+                apiData: { }, // 除翻页外的其他接口参数，但不包含 offset 和 limit
+                count: 30,  // 每页几个
+            }
         },
         // columnNumber: { // 一行显示几个
         //     type: Number,
