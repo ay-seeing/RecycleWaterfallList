3c6ab4d52e6817116db7aefde99492cdf1b6b3d3
yiyang <630999015@qq.com>
2022-08-15 15:44:38 +0800

    feat:代码优化

--- component/RecycleWaterfallList/RecycleWaterfallList.js
+++ component/RecycleWaterfallList/RecycleWaterfallList.js
@@ -2,7 +2,7 @@
  * @Author: yiyang 630999015@qq.com
  * @Date: 2022-07-18 10:49:45
  * @LastEditors: yiyang 630999015@qq.com
- * @LastEditTime: 2022-08-13 11:24:11
+ * @LastEditTime: 2022-08-15 15:41:16
  * @FilePath: /WeChatProjects/ComponentLongList/component/RecycleList/RecycleList.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -292,11 +292,11 @@ Component({
             // 更新请求页码
             this.data._apiData.offset += this.data._apiData.limit;
             
-            this.setData({
-                hasMore: true,
-            }, async ()=>{
+            // this.setData({
+            //     hasMore: true,
+            // }, async ()=>{
                 
-            });
+            // });
 
             // 将新获得的数据进行瀑布流渲染
             await this.waterfallRender(list);
