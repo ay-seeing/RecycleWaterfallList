ffdb888079539ae9ab139c36d5af0a1b2e925c32
yiyang <630999015@qq.com>
2022-08-15 15:55:28 +0800

    feat: 模拟数据添加没有更多模拟

--- component/RecycleWaterfallList/RecycleWaterfallList.js
+++ component/RecycleWaterfallList/RecycleWaterfallList.js
@@ -2,7 +2,7 @@
  * @Author: yiyang 630999015@qq.com
  * @Date: 2022-07-18 10:49:45
  * @LastEditors: yiyang 630999015@qq.com
- * @LastEditTime: 2022-08-15 15:49:49
+ * @LastEditTime: 2022-08-15 15:54:42
  * @FilePath: /WeChatProjects/ComponentLongList/component/RecycleList/RecycleList.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -246,6 +246,14 @@ Component({
                         });
                     }
                     list = testList;
+
+                    if(curentP >= Math.ceil(Math.random()*10 + 10)){
+                        this.setData({
+                            hasMore: false,
+                        }, async ()=>{
+                            
+                        });
+                    }
                 }else{
                     
                     let resp = await app.$fetch({
