e705784ae7f57d29dbf9ccfae10a23eb45f42848
yiyang <630999015@qq.com>
2022-08-16 10:25:03 +0800

    feat: 注释掉多余的代码

--- component/RecycleWaterfallList/RecycleWaterfallList.js
+++ component/RecycleWaterfallList/RecycleWaterfallList.js
@@ -2,7 +2,7 @@
  * @Author: yiyang 630999015@qq.com
  * @Date: 2022-07-18 10:49:45
  * @LastEditors: yiyang 630999015@qq.com
- * @LastEditTime: 2022-08-16 10:18:53
+ * @LastEditTime: 2022-08-16 10:24:41
  * @FilePath: /WeChatProjects/ComponentLongList/component/RecycleList/RecycleList.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -235,9 +235,9 @@ Component({
                 this.data._hasUsedFirstInitData = true;
 
                 this.data._hasMoreMark = initHasMore;
-                this.setData({
-                    hasMore: initHasMore,
-                });
+                // this.setData({
+                //     hasMore: initHasMore,
+                // });
             } else {
                 // 请求接口前设置loading状态
                 this.setData({
