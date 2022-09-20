66a11c2b46a901f26269ba2ace88b9610b94639a
yiyang <630999015@qq.com>
2022-09-20 11:09:52 +0800

    feat : 如果接口报错，则不执行后面的高度计算

--- component/RecycleWaterfallList/RecycleWaterfallList.js
+++ component/RecycleWaterfallList/RecycleWaterfallList.js
@@ -2,7 +2,7 @@
  * @Author: yiyang 630999015@qq.com
  * @Date: 2022-07-18 10:49:45
  * @LastEditors: yiyang 630999015@qq.com
- * @LastEditTime: 2022-08-31 14:34:21
+ * @LastEditTime: 2022-09-20 11:09:42
  * @FilePath: /WeChatProjects/ComponentLongList/component/RecycleList/RecycleList.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -367,6 +367,7 @@ Component({
                         this.setData({
                             hasMore: false,
                         });
+                        return;
                     }
                 }
                 
